df62412458c87107138f3f40fd0955e5bb49dd12
andry2327 <andry2327@gmail.com>
2024-05-24 10:25:10 +0200

    fix

--- test_THOR.py
+++ test_THOR.py
@@ -20,9 +20,7 @@ from utils.options import parse_args_function
 
 # Input parameters
 args = parse_args_function()
-print(f'args.training: {args.training}') # DEBUG
-print(f'args.training: {args.visualize}') # DEBUG
-print(f'args.training: {args.photometric}') # DEBUG
+print(f'args.testing: {args.testing}') # DEBUG
 
 left_hand_faces, right_hand_faces, obj_faces = load_faces(mano_root=args.mano_root, obj_root=args.obj_root)
 
