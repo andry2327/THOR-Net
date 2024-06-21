a84fe3e9472d4831baf45dbaf17b38d52ce1dea2
Andrea Pellegrino <andry2327@gmail.com>
2024-06-21 09:56:15 +0000

    changes in losses plot style

--- utils/plots.py
+++ utils/plots.py
@@ -2,8 +2,11 @@ import re
 import matplotlib.pyplot as plt
 import argparse
 import os
+import numpy as np
 
 plt.rcParams['figure.dpi'] = 300
+FONTSIZE = 18
+
 '''
 def parse_logs(log_file):
     
@@ -70,10 +73,18 @@ def parse_logs(log_file):
                     mesh_loss_3d = float(line_splitted[14].strip(','))
                     photometric = float(line_splitted[17].strip('\n'))
                     epochs_train[epoch] = epoch
-                    losses_2d[epoch] = loss_2d
-                    losses_3d[epoch] = loss_3d
-                    mesh_losses_3d[epoch] = mesh_loss_3d
-                    photometric_losses[epoch] = photometric
+                    if epoch not in losses_2d.keys():
+                        losses_2d[epoch] = []
+                    losses_2d[epoch].append(loss_2d)
+                    if epoch not in losses_3d.keys():
+                        losses_3d[epoch] = []
+                    losses_3d[epoch].append(loss_3d)
+                    if epoch not in mesh_losses_3d.keys():
+                        mesh_losses_3d[epoch] = []
+                    mesh_losses_3d[epoch].append(mesh_loss_3d)
+                    if epoch not in photometric_losses.keys():
+                        photometric_losses[epoch] = []
+                    photometric_losses[epoch].append(photometric)
                 elif '[' not in line and ']' not in line:  # val loss
                     line_splitted = line.split(' ')
                     epoch = int(line_splitted[1].split('/')[0])
@@ -90,10 +101,10 @@ def parse_logs(log_file):
     # Convert dictionaries to lists sorted by epoch
     epochs_train = sorted(epochs_train.values())
     val_epochs = sorted(val_epochs.values())
-    losses_2d = [losses_2d[epoch] for epoch in epochs_train]
-    losses_3d = [losses_3d[epoch] for epoch in epochs_train]
-    mesh_losses_3d = [mesh_losses_3d[epoch] for epoch in epochs_train]
-    photometric_losses = [photometric_losses[epoch] for epoch in epochs_train]
+    losses_2d = [np.mean(losses_2d[epoch]) for epoch in epochs_train]
+    losses_3d = [np.mean(losses_3d[epoch] )for epoch in epochs_train]
+    mesh_losses_3d = [np.mean(mesh_losses_3d[epoch]) for epoch in epochs_train]
+    photometric_losses = [np.mean(photometric_losses[epoch]) for epoch in epochs_train]
     val_losses_2d = [val_losses_2d[epoch] for epoch in val_epochs]
     val_losses_3d = [val_losses_3d[epoch] for epoch in val_epochs]
     val_mesh_losses_3d = [val_mesh_losses_3d[epoch] for epoch in val_epochs]
@@ -104,64 +115,75 @@ def parse_logs(log_file):
 def plot_losses(log_file, out_path=''):
     epochs_train, val_epochs, losses_2d, losses_3d, mesh_losses_3d, photometric_losses, val_losses_2d, val_losses_3d, val_mesh_losses_3d, val_photometric_losses = parse_logs(log_file)
     
-    fig, axs = plt.subplots(3, 2, figsize=(24, 24))
+    fig, axs = plt.subplots(3, 2, figsize=(30, 24))
+
+    # Define fontsize
+    fontsize = FONTSIZE
 
     # Plot training loss 2D
     axs[0, 0].plot(epochs_train, losses_2d, label='Loss 2D', marker='o')
-    axs[0, 0].set_xlabel('Epochs')
-    axs[0, 0].set_ylabel('Loss')
-    axs[0, 0].set_title('Training Loss 2D')
-    axs[0, 0].legend()
+    axs[0, 0].set_xlabel('Epochs', fontsize=fontsize)
+    axs[0, 0].set_ylabel('Loss', fontsize=fontsize)
+    axs[0, 0].set_title('Training Loss 2D', fontsize=fontsize)
+    axs[0, 0].legend(fontsize=fontsize)
     axs[0, 0].grid(True)
-    axs[0, 0].tick_params(axis='x', rotation=45)
+    axs[0, 0].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[0, 0].tick_params(axis='y', labelsize=fontsize)
 
     # Plot training loss 3D and mesh loss 3D
-    axs[1, 0].plot(losses_3d, label='Loss 3D', marker='o')
-    axs[1, 0].plot(mesh_losses_3d, label='Mesh Loss 3D', marker='o')
-    axs[1, 0].set_xlabel('Epochs')
-    axs[1, 0].set_ylabel('Loss')
-    axs[1, 0].set_title('Training Loss 3D and Mesh Loss 3D')
-    axs[1, 0].legend()
+    axs[1, 0].plot(epochs_train, losses_3d, label='Loss 3D', marker='o')
+    axs[1, 0].plot(epochs_train, mesh_losses_3d, label='Mesh Loss 3D', marker='o')
+    axs[1, 0].set_xlabel('Epochs', fontsize=fontsize)
+    axs[1, 0].set_ylabel('Loss', fontsize=fontsize)
+    axs[1, 0].set_title('Training Loss 3D and Mesh Loss 3D', fontsize=fontsize)
+    axs[1, 0].legend(fontsize=fontsize)
     axs[1, 0].grid(True)
-    axs[1, 0].tick_params(axis='x', rotation=45)
+    axs[1, 0].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[1, 0].tick_params(axis='y', labelsize=fontsize)
 
     # Plot training photometric loss
-    axs[2, 0].plot(photometric_losses, label='Photometric Loss', marker='o')
-    axs[2, 0].set_xlabel('Epochs')
-    axs[2, 0].set_ylabel('Loss')
-    axs[2, 0].set_title('Training Photometric Loss')
-    axs[2, 0].legend()
+    axs[2, 0].plot(epochs_train, photometric_losses, label='Photometric Loss', marker='o')
+    axs[2, 0].set_xlabel('Epochs', fontsize=fontsize)
+    axs[2, 0].set_ylabel('Loss', fontsize=fontsize)
+    axs[2, 0].set_title('Training Photometric Loss', fontsize=fontsize)
+    axs[2, 0].legend(fontsize=fontsize)
     axs[2, 0].grid(True)
-    axs[2, 0].tick_params(axis='x', rotation=45)
+    axs[2, 0].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[2, 0].tick_params(axis='y', labelsize=fontsize)
 
     # Plot validation loss 2D
     axs[0, 1].plot(val_epochs, val_losses_2d, label='Validation Loss 2D', marker='x')
-    axs[0, 1].set_xlabel('Epochs')
-    axs[0, 1].set_ylabel('Loss')
-    axs[0, 1].set_title('Validation Loss 2D')
-    axs[0, 1].legend()
+    axs[0, 1].set_xlabel('Epochs', fontsize=fontsize)
+    axs[0, 1].set_ylabel('Loss', fontsize=fontsize)
+    axs[0, 1].set_title('Validation Loss 2D', fontsize=fontsize)
+    axs[0, 1].legend(fontsize=fontsize)
     axs[0, 1].grid(True)
-    axs[0, 1].tick_params(axis='x', rotation=45)
+    axs[0, 1].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[0, 1].tick_params(axis='y', labelsize=fontsize)
 
     # Plot validation loss 3D and mesh loss 3D
     axs[1, 1].plot(val_epochs, val_losses_3d, label='Validation Loss 3D', marker='x')
     axs[1, 1].plot(val_epochs, val_mesh_losses_3d, label='Validation Mesh Loss 3D', marker='x')
-    axs[1, 1].set_xlabel('Epochs')
-    axs[1, 1].set_ylabel('Loss')
-    axs[1, 1].set_title('Validation Loss 3D and Mesh Loss 3D')
-    axs[1, 1].legend()
+    axs[1, 1].set_xlabel('Epochs', fontsize=fontsize)
+    axs[1, 1].set_ylabel('Loss', fontsize=fontsize)
+    axs[1, 1].set_title('Validation Loss 3D and Mesh Loss 3D', fontsize=fontsize)
+    axs[1, 1].legend(fontsize=fontsize)
     axs[1, 1].grid(True)
-    axs[1, 1].tick_params(axis='x', rotation=45)
+    axs[1, 1].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[1, 1].tick_params(axis='y', labelsize=fontsize)
 
     # Plot validation photometric loss
     axs[2, 1].plot(val_epochs, val_photometric_losses, label='Validation Photometric Loss', marker='x')
-    axs[2, 1].set_xlabel('Epochs')
-    axs[2, 1].set_ylabel('Loss')
-    axs[2, 1].set_title('Validation Photometric Loss')
-    axs[2, 1].legend()
+    axs[2, 1].set_xlabel('Epochs', fontsize=fontsize)
+    axs[2, 1].set_ylabel('Loss', fontsize=fontsize)
+    axs[2, 1].set_title('Validation Photometric Loss', fontsize=fontsize)
+    axs[2, 1].legend(fontsize=fontsize)
     axs[2, 1].grid(True)
-    axs[2, 1].tick_params(axis='x', rotation=45)
+    axs[2, 1].tick_params(axis='x', rotation=45, labelsize=fontsize)
+    axs[2, 1].tick_params(axis='y', labelsize=fontsize)
 
+    plt.subplots_adjust(hspace=20, wspace=10, top=0.9, bottom=0.1)
+    
     file_name = f'Loss_plots--{os.path.basename(log_file).strip(".txt")}.png'
     if not os.path.exists(out_path):
         os.makedirs(out_path)
